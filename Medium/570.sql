553b2847cb24c1515d31b640a492b039a3d3837c
Katerina <katerina.chikova@gmail.com>
2024-04-10 12:24:22 +0300

    570

--- Medium/570.sql
+++ Medium/570.sql
@@ -1,4 +1,10 @@
+--1
 SELECT man.name
 FROM Employee man LEFT JOIN Employee emp ON man.id=emp.managerId
 GROUP BY man.id, man.name
 HAVING COUNT(1)>=5
+
+--2
+SELECT name
+FROM Employee 
+WHERE ID IN (SELECT managerId FROM Employee GROUP BY managerId HAVING COUNT(1)>=5)
